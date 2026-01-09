339f318eeeb9dee276cc4217c1d3daa5f5951b74
Eduardo Barroso <edbar42@proton.me>
2026-01-08 22:11:35 -0300

    feat: add top nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,25 @@ const geist = Geist({
 	variable: "--font-geist-sans",
 });
 
+function TopNav() {
+	return (
+		<div className="w-full bg-gradient-to-r from-orange-900 via-orange-600 to-orange-900 pb-[2px]">
+			<nav className="flex w-full items-center justify-between bg-[#0a0604] p-4 text-xl">
+				<span>Your gallery</span> <span>Sign In</span>
+			</nav>
+		</div>
+	);
+}
+
 export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html className={`${geist.variable}`} lang="en">
-			<body>{children}</body>
+			<body>
+				<TopNav />
+				{children}
+			</body>
 		</html>
 	);
 }
